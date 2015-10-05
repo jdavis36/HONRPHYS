cda825194f71fc78c91813a040eb272e7419f32e
Jeffrey <jd43476@gmail.com>
2015-10-05 19:56:40 -0400

    First commit

--- HW4_1.cpp
+++ HW4_1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int factorial = 1;
-  for(int n = 0; n < 10; n++)
-    { factorial = 1;
-      for(int i = 0; i < n ; i++){
-	factorial += factorial * i;
+  int factorial = 1;  //Since factorial 0 = 1 i wanted factorials value to start at 1
+  for(int n = 0; n < 10; n++) //loops through values of n from 0 to 10
+    { factorial = 1; //resets factorial to 1
+      for(int i = 0; i < n ; i++){//loops through values of i from 0 to n
+	factorial += factorial * i; //sets factorial equal to iteself * the next integer
       }
-      cout << factorial <<endl;
+      cout << factorial <<endl; // prints out the factorial of n
     }
 }
--- HW4_2.cpp
+++ HW4_2.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-  for(int i = 1; i<101; i++)
+  for(int i = 1; i<101; i++) // loops through integers from 1 to 100
     {
-      cout <<"factors of " <<i;
-      for( int k = 1 ; k < i; k++)
+      cout <<"factors of " <<i; // prints out factors of the integer
+      for( int k = 1 ; k < i; k++)// loops from 1 to i
 	{
-	  if( i % k == 0) 
+	  if( i % k == 0) //checks to see if i is divisible by k  
 	    {
-	      cout<<" "<<k<<"," ; 
+	      cout<<" "<<k<<"," ; //prints out the factors
 	    }
 	}
-      cout<<endl;
+      cout<<endl; // ends the line
     }
+  return 0;
 }
