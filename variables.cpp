fe49d4f1aeaf8e48e55a44f99fe35e59c73de914
Jeffrey <jd43476@gmail.com>
2015-10-04 23:24:42 -0400

    First commit

---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "helloworld" << endl;
+  int i =2;
+  cout << "i = " <<i<<endl;
+
+  double a=3.3;
+  cout << "a = " <<a<<endl;
+  int j = a*i;
+  cout << "a*i = "<<j<<endl;
+  return 0;
+}
